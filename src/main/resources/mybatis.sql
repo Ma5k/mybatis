c80291ec00144ff9e6a71ad8ff95a0a7c2ec86d7
Mask <2421697126@qq.com>
2019-08-02 18:21:33 +0800

    加上导出的sql

---
+++ src/main/resources/mybatis.sql
@@ -0,0 +1,90 @@
+﻿# Host: localhost  (Version 5.7.21)
+# Date: 2019-08-02 18:17:38
+# Generator: MySQL-Front 6.1  (Build 1.21)
+
+
+#
+# Structure for table "items"
+#
+
+DROP TABLE IF EXISTS `items`;
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL COMMENT '商品名称',
+  `price` float(10,1) NOT NULL COMMENT '商品定价',
+  `detail` text COMMENT '商品描述',
+  `pic` varchar(64) DEFAULT NULL COMMENT '商品图片',
+  `createtime` datetime NOT NULL COMMENT '生产日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "items"
+#
+
+INSERT INTO `items` VALUES (1,'可乐',3.0,NULL,NULL,'2019-01-01 00:00:00'),(2,'牛奶',6.0,NULL,NULL,'2019-01-02 00:00:00'),(3,'巧克力',8.0,NULL,NULL,'2019-04-01 00:00:00');
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL COMMENT '用户名称',
+  `birthday` date DEFAULT NULL COMMENT '生日',
+  `sex` char(1) DEFAULT NULL COMMENT '性别',
+  `address` varchar(256) DEFAULT NULL COMMENT '地址',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
+INSERT INTO `user` VALUES (1,'mask','1995-01-10','男','武汉');
+
+#
+# Structure for table "orders"
+#
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL COMMENT '下单用户id',
+  `number` varchar(32) NOT NULL COMMENT '订单号',
+  `createtime` datetime NOT NULL COMMENT '创建订单时间',
+  `note` varchar(100) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`),
+  KEY `FK_orders_1` (`user_id`),
+  CONSTRAINT `FK_orders_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "orders"
+#
+
+INSERT INTO `orders` VALUES (1,1,'101','2019-08-01 00:00:00','第一订单');
+
+#
+# Structure for table "orderdetail"
+#
+
+DROP TABLE IF EXISTS `orderdetail`;
+CREATE TABLE `orderdetail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `orders_id` int(11) NOT NULL COMMENT '订单id',
+  `items_id` int(11) NOT NULL COMMENT '商品id',
+  `items_num` int(11) DEFAULT NULL COMMENT '商品购买数量',
+  PRIMARY KEY (`id`),
+  KEY `FK_orderdetail_1` (`orders_id`),
+  KEY `FK_orderdetail_2` (`items_id`),
+  CONSTRAINT `FK_orderdetail_1` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_orderdetail_2` FOREIGN KEY (`items_id`) REFERENCES `items` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "orderdetail"
+#
+
+INSERT INTO `orderdetail` VALUES (1,1,3,2),(2,1,1,3);
